3182b96f7bc87b939b93878e7bc48b04dcd49fe3
DoctorDiLauro <chrisdilauro@Chriss-MacBook-Pro-3.local>
2017-05-02 13:04:57 -0400

    fixed unnecessary loops

---
+++ js/main.js
@@ -0,0 +1,60 @@
+var newLetter = prompt('Enter a letter');
+var count;
+
+for (count = 0; count < 1; count++) {
+  document.write(newLetter);
+}
+
+document.write('<br>');
+
+for (count = 0; count < 2; count++) {
+  document.write(newLetter);
+}
+
+document.write('<br>');
+
+for (count = 0; count < 3; count++) {
+  document.write(newLetter);
+}
+
+document.write('<br>');
+
+for (count = 0; count < 4; count++) {
+  document.write(newLetter);
+}
+
+document.write('<br>')
+
+for (count = 0; count < 5; count++) {
+  document.write(newLetter);
+}
+
+document.write('<br>')
+
+for (count = 0; count < 6; count++) {
+  document.write(newLetter);
+}
+
+document.write('<br>')
+
+for (count = 0; count < 7; count++) {
+  document.write(newLetter);
+}
+
+document.write('<br>')
+
+for (count = 0; count < 8; count++) {
+  document.write(newLetter);
+}
+
+document.write('<br>')
+
+for (count = 0; count < 9; count++) {
+  document.write(newLetter);
+}
+
+document.write('<br>')
+
+for (count = 0; count < 10; count++) {
+  document.write(newLetter);
+}
